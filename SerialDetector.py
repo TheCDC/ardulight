3c7e9cc206eae2ff1027ffe7ad3e27c9c05c687e
Christopher Chen <christoipher.chen@gmail.com>
2017-07-05 23:40:33 -0700

    encoding

--- SerialDetector.py
+++ SerialDetector.py
@@ -4,33 +4,34 @@ import serial
 
 
 def serial_ports():
-	""" Lists serial port names
+    """ Lists serial port names
 
-		:raises EnvironmentError:
-			On unsupported or unknown platforms
-		:returns:
-			A list of the serial ports available on the system
-	"""
-	if sys.platform.startswith('win'):
-		ports = ['COM%s' % (i + 1) for i in range(256)]
-	elif sys.platform == 'linux' or sys.platform.startswith('cygwin'):
-		# this excludes your current terminal "/dev/tty"
-		ports = glob.glob('/dev/ttyACM[0-9]*') + glob.glob('/dev/rfcom[A-Za-z]*')
-	elif sys.platform.startswith('darwin'):
-		ports = glob.glob('/dev/tty.*')
-	else:
-		raise EnvironmentError('Unsupported platform')
+            :raises EnvironmentError:
+                    On unsupported or unknown platforms
+            :returns:
+                    A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform == 'linux' or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/ttyACM[0-9]*') + \
+            glob.glob('/dev/rfcom[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
 
-	result = []
-	for port in ports:
-		try:
-			s = serial.Serial(port)
-			s.close()
-			result.append(port)
-		except (OSError, serial.SerialException):
-			pass
-	return result
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
 
 
 if __name__ == '__main__':
-	print(serial_ports())
+    print(serial_ports())
